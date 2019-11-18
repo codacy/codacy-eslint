1c7f50daf9177dd29e6d301ac0973043982c81ee
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2019-11-18 18:16:53 +0100

    clean: Launch prettier and eslint at commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true
+  },
+  extends: ["prettier"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {}
+}
