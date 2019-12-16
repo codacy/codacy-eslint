d0ecdc9a350b6eef0aaf02971a1f315495492d20
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2019-12-16 17:32:41 +0100

    clean: Remove WithDefault type

--- src/NamedParameters.ts
+++ src/NamedParameters.ts
@@ -2,14 +2,12 @@ import { JSONSchema4 } from "json-schema"
 import { flatMap, toPairs } from "lodash"
 import { PatternsParameter } from "./model/Patterns"
 
-type WithDefault = { default: any }
-
 export function fromSchemaArray(objects: JSONSchema4[]): PatternsParameter[] {
   return flatMap(objects, o => {
     const pairs = toPairs(o.properties)
     const haveDefault = pairs.filter(
       ([k, v]) => v && v.hasOwnProperty("default")
-    ) as [string, WithDefault][]
+    )
     return haveDefault.map(([k, v]) => new PatternsParameter(k, v.default))
   })
 }
