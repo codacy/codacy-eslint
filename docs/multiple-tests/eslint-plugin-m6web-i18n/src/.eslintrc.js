284ac6b460598330530933e1abf4e53e98d87a13
José Miguel  Melo <josemiguelmelo94@gmail.com>
2020-01-03 11:35:27 +0000

    Added eslint-plugin-m6web-i18n plugin

---
+++ docs/multiple-tests/eslint-plugin-m6web-i18n/src/.eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+    plugins: [
+        "m6web-i18n"
+    ],
+    rules: {
+        "m6web-i18n/no-unknown-key": "error",
+        "m6web-i18n/no-unknown-key-secondary-langs": "warn",
+        "m6web-i18n/no-text-as-children": ["error", { "ignorePattern": "^\\s?[/.]\\s?$" }],
+        "m6web-i18n/no-text-as-attribute": ["error", { "attributes": ["alt", "title"] }],
+        "m6web-i18n/interpolation-data": ["error", { "interpolationPattern": "\\{\\.+\\}" }]
+    },
+    settings: {
+        "i18n": {
+            // Used in 'no-unknown-key' rule
+            "principalLangs": [
+                {
+                    "name": "fr",
+                    "translationPath": "fr.json"
+                }
+            ],
+            // Used in 'no-unknown-key-secondary-langs' rule
+            "secondaryLangs": [
+                {
+                    "name": "en",
+                    "translationPath": "en.json"
+                }
+            ],
+            // Translate function
+            "functionName": "t",
+            "pluralizedKeys": ["one", "other"],
+            "translationsCacheTTL": 300
+        }
+    }
+};
