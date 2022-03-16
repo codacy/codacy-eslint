0aee37cd05eae45205bba26f73ecddc3e4c1923d
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2022-03-16 15:15:30 +0100

    fix: Add default parameters for `ember/no-restricted-property-modifications`

--- src/rulesToUnnamedParametersDefaults.ts
+++ src/rulesToUnnamedParametersDefaults.ts
@@ -51,6 +51,7 @@ export const rulesToUnnamedParametersDefaults = new Map<string, any>([
 
 export class rulesNamedParametersAndDefaults {
   private static readonly array: [string, string, any][] = [
+    ["ember/no-restricted-property-modifications", "properties", [""]],
     ["ember/no-restricted-service-injections", "paths", [""]],
     ["ember/no-restricted-service-injections", "services", [""]],
     [
