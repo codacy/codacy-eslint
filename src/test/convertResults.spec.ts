705553477ce3f9831d1fab4df6f9dc7bd2d84e08
stefanvacareanu7 <91726880+stefanvacareanu7@users.noreply.github.com>
2022-05-31 15:50:42 +0300

    Remove suppressedMessages

--- src/test/convertResults.spec.ts
+++ src/test/convertResults.spec.ts
@@ -34,7 +34,7 @@ describe("convertResults", () => {
         fixableErrorCount: 0,
         fixableWarningCount: 0,
         usedDeprecatedRules: [],
-        suppressedMessages: [],
+
       },
     ]
 
