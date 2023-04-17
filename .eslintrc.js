66d7d41a624dc625821c72a8b01a896b7162d7a6
David Marinho <david.marinho@codacy.com>
2023-04-17 17:27:07 +0100

    fix circle ci issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
     sourceType: "module",
     projects: ["./tsconfig.json"],
   },
-  plugins: ["@typescript-eslint", "simple-import-sort", "unused-imports","jest"],
+  plugins: ["@typescript-eslint", "simple-import-sort", "unused-imports"],
   rules: {
     "simple-import-sort/imports": "error",
     '@typescript-eslint/unbound-method': 'off'
