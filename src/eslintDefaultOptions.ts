66d10716306eda91bdd602b39e6a3eccbc351a0e
Vítor Pires <vitor.pires@codacy.com>
2023-07-28 17:28:41 +0100

    fix: multiple-tests

--- src/eslintDefaultOptions.ts
+++ src/eslintDefaultOptions.ts
@@ -35,70 +35,70 @@ export const defaultOptions: ESLint.Options = {
       embertest: true,
     },
     globals: {
-      document: "readonly",
-      navigator: "readonly",
-      window: "readonly",
+      // document: "readonly",
+      // navigator: "readonly",
+      // window: "readonly",
 
-      // ECMAScript
-      ArrayBuffer: "readonly",
-      Atomics: "readonly",
-      BigInt: "readonly",
-      BigInt64Array: "readonly",
-      BigUint64Array: "readonly",
-      DataView: "readonly",
-      Float32Array: "readonly",
-      Float64Array: "readonly",
-      Int16Array: "readonly",
-      Int32Array: "readonly",
-      Int8Array: "readonly",
-      Map: "readonly",
-      Promise: "readonly",
-      Proxy: "readonly",
-      Reflect: "readonly",
-      Set: "readonly",
-      SharedArrayBuffer: "readonly",
-      Symbol: "readonly",
-      Uint16Array: "readonly",
-      Uint32Array: "readonly",
-      Uint8Array: "readonly",
-      Uint8ClampedArray: "readonly",
-      WeakMap: "readonly",
-      WeakSet: "readonly",
+      // // ECMAScript
+      // ArrayBuffer: "readonly",
+      // Atomics: "readonly",
+      // BigInt: "readonly",
+      // BigInt64Array: "readonly",
+      // BigUint64Array: "readonly",
+      // DataView: "readonly",
+      // Float32Array: "readonly",
+      // Float64Array: "readonly",
+      // Int16Array: "readonly",
+      // Int32Array: "readonly",
+      // Int8Array: "readonly",
+      // Map: "readonly",
+      // Promise: "readonly",
+      // Proxy: "readonly",
+      // Reflect: "readonly",
+      // Set: "readonly",
+      // SharedArrayBuffer: "readonly",
+      // Symbol: "readonly",
+      // Uint16Array: "readonly",
+      // Uint32Array: "readonly",
+      // Uint8Array: "readonly",
+      // Uint8ClampedArray: "readonly",
+      // WeakMap: "readonly",
+      // WeakSet: "readonly",
 
-      // ECMAScript (experimental)
-      globalThis: "readonly",
+      // // ECMAScript (experimental)
+      // globalThis: "readonly",
 
-      // ECMA-402
-      Intl: "readonly",
+      // // ECMA-402
+      // Intl: "readonly",
 
-      // Web Standard
-      TextDecoder: "readonly",
-      TextEncoder: "readonly",
-      URL: "readonly",
-      URLSearchParams: "readonly",
-      WebAssembly: "readonly",
-      clearInterval: "readonly",
-      clearTimeout: "readonly",
-      console: "readonly",
-      queueMicrotask: "readonly",
-      setInterval: "readonly",
-      setTimeout: "readonly",
+      // // Web Standard
+      // TextDecoder: "readonly",
+      // TextEncoder: "readonly",
+      // URL: "readonly",
+      // URLSearchParams: "readonly",
+      // WebAssembly: "readonly",
+      // clearInterval: "readonly",
+      // clearTimeout: "readonly",
+      // console: "readonly",
+      // queueMicrotask: "readonly",
+      // setInterval: "readonly",
+      // setTimeout: "readonly",
 
-      // Node.js
-      Buffer: "readonly",
-      GLOBAL: "readonly",
-      clearImmediate: "readonly",
-      global: "readonly",
-      process: "readonly",
-      root: "readonly",
-      setImmediate: "readonly",
+      // // Node.js
+      // Buffer: "readonly",
+      // GLOBAL: "readonly",
+      // clearImmediate: "readonly",
+      // global: "readonly",
+      // process: "readonly",
+      // root: "readonly",
+      // setImmediate: "readonly",
 
-      // Backbone
-      Backbone: false,
-      _: false,
+      // // Backbone
+      // Backbone: false,
+      // _: false,
 
-      // Cypress
-      "cypress/globals": true,
+      // // Cypress
+      // "cypress/globals": true,
     },
     ignorePatterns: [
       "/src/**/node_modules/**/*",
@@ -135,7 +135,6 @@ export const defaultOptions: ESLint.Options = {
           alwaysTryTypes: true
         },
         webpack: true,
-        caseSensitive: false
       },
       jest: {
         version: 26,
