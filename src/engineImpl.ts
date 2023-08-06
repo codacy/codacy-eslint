36d067c125657a4b95bb386daa9da110159e60fa
Vítor Pires <vitor.pires@codacy.com>
2023-08-06 23:35:52 +0100

    refactor; bump packages;

--- src/engineImpl.ts
+++ src/engineImpl.ts
@@ -12,13 +12,14 @@ export const engineImpl: Engine = async function (
   debug("engine: starting")
   const srcDirPath = "/src"
   const tsconfigFile = "tsconfig.json"
+  const nFilesPerChunk = 20
 
   const [options, files] = await configCreator(
     srcDirPath,
     tsconfigFile,
     codacyrc
   )
-  
+
   if (DEBUG) {
     debug("engine: " + files.length + " files (or patterns) to process in \"" + srcDirPath + "\" with below config")
     debugJson(options)
@@ -26,22 +27,33 @@ export const engineImpl: Engine = async function (
 
   //TODO: create file eslintrc options if it doesn't exist in root /src
   const eslint = new ESLint(options)
-  
+
   //TODO: check why should this be instantiated or if it should be used instead for performance sake...
   // const linter = new Linter(options)
 
   debug("engine: linting")
   //TODO: RFC intercept results - try-catch - and check if there is some missing module
   // then maybe try to install it on demand...? have to think about ui implications...
-  
-  //TODO: try linting smaller batches (maybe 20?) of files here to solve heap memory errors
-    
-  const lintResults = await eslint.lintFiles(files)
-  
+
+  //TODO: validate code to lint smaller batches of files here
+  // to solve heap memory errors and increase performance
+  let chunksOfFiles = chunk(files, nFilesPerChunk)
+  let lintResults: ESLint.LintResult[];
+  for (let chunkOfFiles of chunksOfFiles) {
+    lintResults.concat(await eslint.lintFiles(chunkOfFiles))
+  }
+
   if (DEBUG) {
     let nIssues = 0
     for (let lintResult of lintResults) {
-      debug("engine: below specific config for \"" + lintResult.filePath + "\"\n" + JSON.stringify(await eslint.calculateConfigForFile(lintResult.filePath)))
+      debug(
+        "engine: below specific config for \"" +
+        lintResult.filePath +
+        "\"\n" +
+        JSON.stringify(
+          await eslint.calculateConfigForFile(lintResult.filePath)
+        )
+      )
       nIssues += lintResult.messages.length
     }
     debug("engine: " + lintResults.length + " files linted and " + nIssues + " issues found")
@@ -49,3 +61,8 @@ export const engineImpl: Engine = async function (
 
   return convertResults(lintResults).map((r) => r.relativeTo(srcDirPath))
 }
+
+const chunk = (arr: any[], size: number) =>
+  Array.from({ length: Math.ceil(arr.length / size) }, (_: any, i: number) =>
+    arr.slice(i * size, i * size + size)
+  );
