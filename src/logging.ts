3beb6d676d6ac67d02e75ebd4f04e707819d492c
Vítor Pires <vitor.pires@codacy.com>
2023-09-05 10:51:20 +0100

    reduce chunk size; reorganize code to add clarity;

--- src/logging.ts
+++ src/logging.ts
@@ -4,27 +4,27 @@
 export const DEBUG = process.env.DEBUG
 
 export function debug(msg: String): void {
-    if (DEBUG) {
-        console.log("[DEBUG] " + msg)
-    }
+  if (DEBUG) {
+    console.debug("[DEBUG] " + msg)
+  }
 }
 
-export function debugEach<T>(arr: Array<T> | undefined): void {
-    arr?.forEach((value) => {
-        debug(value.toString())
-    })
+export function debugEach<T>(arr?: Array<T>): void {
+  arr?.forEach(value => {
+    debug(value.toString())
+  })
 }
 
 export function debugJson(jsonObject: any): void {
-    debug(JSON.stringify(jsonObject))
+  debug(JSON.stringify(jsonObject, null, 2))
 }
 
 export function debugWhen(cond: Boolean, msg: string): void {
-    if (cond) {
-        debug(msg)
-    }
+  if (cond) {
+    debug(msg)
+  }
 }
 
 export function debugEither(cond: Boolean, msgTrue: string, msgFalse: string): void {
-    (cond) ? debug(msgTrue) : debug(msgFalse)
+  (cond) ? debug(msgTrue) : debug(msgFalse)
 }
