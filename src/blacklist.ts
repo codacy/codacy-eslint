7b8a0c31fed5511243e40088619b48c1bb3832d3
Vítor Pires <vitor.pires@codacy.com>
2023-11-28 20:07:53 +0000

    fix tests;

--- src/blacklist.ts
+++ src/blacklist.ts
@@ -1,5 +1,5 @@
 const blacklistRegexes: RegExp[] = [
-  /@lwc\/lwc\/no-unexpected-wire-adapter-usages/,
+  /@lwc\/lwc\/no-.*/,
   /@typescript-eslint\/lines-between-class-members/,
   /canonical\/id-match/,
   /ember\/no-restricted-property-modifications/,
