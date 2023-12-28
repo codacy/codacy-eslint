5a5ceaa2f1ef70f10242c2f85a3e286930198448
Vítor Pires <vitor.pires@codacy.com>
2023-12-28 02:50:12 +0000

    fix docs

--- src/blacklist.ts
+++ src/blacklist.ts
@@ -4,6 +4,7 @@ const blacklistRegexes: RegExp[] = [
   /@typescript-eslint\/lines-between-class-members/,
   /angular\/service-name/,
   /ember\/no-restricted-property-modifications/,
+  /eslint-plugin\/require-meta-docs-description/,
   /functional\/functional-parameters/,
   /functional\/immutable-data/,
   /functional\/no-conditional-statements/,
