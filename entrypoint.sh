2392f9665e8f3a871696e47678621e7b20126093
Vítor Pires <vitor.pires@codacy.com>
2024-02-20 13:14:00 +0000

    fix script syntax

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-max_ram=`cat /sys/fs/cgroup/memory/memory.limit_in_bytes`
-max_ram=`expr $max_ram / 1024 / 1024`
-old_space_size=`expr $max_ram - 356`
+max_ram=$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes)
+max_ram=$((max_ram / 1024 / 1024))
+old_space_size=$((max_ram - 356))
 
-exec node --max-old-space-size=$old_space_size --max-semi-space-size=32 --optimize_for_size --gc_interval=100 --v8-pool-size=0 --use-largepages=silent /dist/src/index.js 
+exec node --max-old-space-size="$old_space_size" --max-semi-space-size=32 --optimize_for_size --gc_interval=100 --v8-pool-size=0 --use-largepages=silent /dist/src/index.js 
