caea9f26c5fffc400559c551a2d555730af1f12a
david.marinho@codacy.com <david.marinho@codacy.com>
2025-01-24 16:09:17 +0000

    tests

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-max_ram=$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes)
+#max_ram=$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes)
+max_ram = 1000000000
 max_ram=$((max_ram / 1024 / 1024))
 old_space_size=$((max_ram - 356))
 
