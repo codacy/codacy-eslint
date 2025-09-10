d3895d6f7686ab85e7d84cc8bcd1f8f7c4af2849
DMarinhoCodacy <102817200+DMarinhoCodacy@users.noreply.github.com>
2025-09-10 16:52:56 +0100

    Performance improvements (#4892)
    * Performance improvements Node

--- src/convertResults.ts
+++ src/convertResults.ts
@@ -5,30 +5,37 @@ import { isBlacklisted } from "./blacklist"
 import { computeSuggestion } from "./computeSuggestion"
 import { patternIdToCodacy } from "./model/patterns"
 
-export function convertResults (eslintResults: ESLint.LintResult[]): ToolResult[] {
+export function convertResults(eslintResults: ESLint.LintResult[]): ToolResult[] {
   const results: ToolResult[] = []
-  eslintResults.forEach((result) => {
-    const { "filePath": filename, messages } = result
 
-    if (result.fatalErrorCount > 0) {
-      results.push(new FileError(filename, messages.filter((m) => m.fatal).map((m) => m.message).join("\\n")))
-      return
+  for (const result of eslintResults) {
+    const filename = result.filePath
+    const { messages, fatalErrorCount, source } = result
+
+    if (fatalErrorCount > 0) {
+      const fatalMessages = messages
+        .filter((m) => m.fatal)
+        .map((m) => m.message)
+        .join("\n")
+      results.push(new FileError(filename, fatalMessages))
+      continue
     }
 
-    const issues = messages
-      .filter((r) => r.ruleId && !isBlacklisted(r.ruleId))
-      .map((m) => {
-        const { ruleId, line, endLine, message, fix, suggestions } = m
-        const patternId = patternIdToCodacy(ruleId)
-        const suggestion
-          = process.env.SUGGESTIONS === "true" && result.source
-            ? computeSuggestion(result.source, line, endLine, fix, suggestions)
-            : undefined
+    const hasSuggestions = process.env.SUGGESTIONS === "true" && typeof source === "string"
 
-        return new Issue(filename, message, patternId, line, suggestion)
-      })
+    for (const m of messages) {
+      if (!m.ruleId || isBlacklisted(m.ruleId)) continue
 
-    results.push(...issues)
-  })
+      const patternId = patternIdToCodacy(m.ruleId)
+      const suggestion
+        = hasSuggestions
+          ? computeSuggestion(source!, m.line, m.endLine, m.fix, m.suggestions)
+          : undefined
+
+      if (suggestion) console.log(`Suggestion for ${filename}:${m.line} - ${patternId}: ${suggestion}`)
+      results.push(new Issue(filename, m.message, patternId, m.line, suggestion))
+    }
+  }
+
   return results
 }
